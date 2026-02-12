8977cf0d196021fcd644c49660515fa64426cce9
Adarsh <codewithadarsh007@gmail.com>
2026-02-13 00:00:58 +0530

    Youtube Project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+ 
+      <div>
+        <h1 className="text-amber-950 text-9xl">Hi</h1>
+      </div>
+      
+  
+  )
+}
+
+export default App
