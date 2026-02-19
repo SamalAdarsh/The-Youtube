24fee5bef38a3644b6d6b7f0e7c4a9f728307575
Adarsh <codewithadarsh007@gmail.com>
2026-02-19 11:36:13 +0530

    Watch Page

--- src/components/WatchPage.jsx
+++ src/components/WatchPage.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { closeMenu } from "../utils/appSlice";
+import { useParams, useSearchParams } from "react-router";
 
 const WatchPage = () => {
-  return (
-    <div>Watch Page</div>
-  )
-}
+  const dispatch = useDispatch();
 
-export default WatchPage
+  const params = useParams();
+  console.log(params);
+
+  const [searchParams] = useSearchParams();
+  console.log(searchParams.get("v"));
+
+  useEffect(() => {
+    dispatch(closeMenu());
+  }, []);
+
+  return (
+    <div className="p-4">
+      <iframe
+        width="1200"
+        height="600"
+        src={"https://www.youtube.com/embed/" + searchParams.get("v") }
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+};
+
+export default WatchPage;
